e4ae3fe563dab384d290bfa4d9c5bfa91d93a737
ikyriak <44930763+ikyriak@users.noreply.github.com>
2020-09-12 17:10:36 +0300

    Improve hello wolrd example

--- TutorialWebApi/Controllers/HelloWorldController.cs
+++ TutorialWebApi/Controllers/HelloWorldController.cs
@@ -16,32 +16,41 @@ namespace TutorialWebApi.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Hello", "World!" };
         }
 
         // GET api/<HelloWorldController>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return "value:" + id.ToString();
         }
 
         // POST api/<HelloWorldController>
         [HttpPost]
         public void Post([FromBody] string value)
         {
+            // Validate Input.
+
+            // Create Resource, e.g. in our database.
         }
 
         // PUT api/<HelloWorldController>/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
+            // Validate Input.
+
+            // Update/Replace the Resource, e.g. in our database.
         }
 
         // DELETE api/<HelloWorldController>/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            // Validate Input.
+
+            // Delete the Resource, e.g. in our database.
         }
     }
 }
